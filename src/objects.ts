858a3b63ca9fba764a48bfaaf8d355546ad7a617
Pier Fumagalli <pier@usrz.com>
2021-11-13 01:22:48 +0100

    Collect up errors

--- src/objects.ts
+++ src/objects.ts
@@ -11,6 +11,7 @@ import {
 import { assert, getValidator, isPrimitive, isValidator } from './utilities'
 
 import { any } from './primitives'
+import { ValidationErrorBuilder } from './errors'
 
 /* ========================================================================== *
  * OBJECT VALIDATOR                                                           *
@@ -52,33 +53,52 @@ export function object(schema: Schema = {}): SchemaValidator<any, Schema> {
       assert(typeof value == 'object', 'Value is not an "object"')
       assert(value !== null, 'Value is "null"')
 
+      const builder = new ValidationErrorBuilder()
       const clone: Record<string, any> = {}
 
       for (const key of Object.keys(requiredProperties)) {
         const validator = requiredProperties[key]
 
-        assert(value[key] !== undefined, 'Required value is "undefined"')
-        clone[key] = validator.validate(value[key])
+        try {
+          assert(value[key] !== undefined, 'Required value is "undefined"')
+          clone[key] = validator.validate(value[key])
+        } catch (error) {
+          builder.record(key, error)
+        }
       }
 
       for (const key of Object.keys(optionalProperties)) {
         const validator = requiredProperties[key]
 
-        if (value[key] !== undefined) clone[key] = validator.validate(value[key])
+        try {
+          if (value[key] !== undefined) clone[key] = validator.validate(value[key])
+        } catch (error) {
+          builder.record(key, error)
+        }
       }
 
       for (const key of neverProperties) {
-        assert(value[key] == undefined, 'Forbidden property found')
+        try {
+          assert(value[key] == undefined, 'Forbidden property found')
+        } catch (error) {
+          builder.record(key, error)
+        }
       }
 
       if (additionalProperties) {
         const cloned = Object.keys(clone)
         const keys = Object.keys(value).filter((k) => ! cloned.includes(k))
 
-        keys.filter((k) => value[k] !== undefined)
-            .forEach((key) => clone[key] = additionalProperties.validate(value[key]))
+        keys.filter((k) => value[k] !== undefined).forEach((key) => {
+          try {
+            clone[key] = additionalProperties.validate(value[key])
+          } catch (error) {
+            builder.record(key, error)
+          }
+        })
       }
 
+      builder.assert()
       return clone
     },
   }
