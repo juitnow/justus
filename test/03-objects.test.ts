536285e7bc75b4a03ad66dcfe530235a849e00c1
Pier Fumagalli <pier@usrz.com>
2021-11-17 00:22:02 +0100

    Complex object validation

--- test/03-objects.test.ts
+++ test/03-objects.test.ts
@@ -1,4 +1,4 @@
-import { ValidationError, object, validate, string, number } from '../src'
+import { ValidationError, object, validate, string, number, array } from '../src'
 import { expect } from 'chai'
 
 describe('Array validators', () => {
@@ -12,13 +12,13 @@ describe('Array validators', () => {
     expect(() => validate(object, 123))
         .to.throw(ValidationError, 'Found 1 validation error')
         .with.property('errors').to.eql([
-          { key: '', message: 'Value is not an "object"' },
+          { path: [], message: 'Value is not an "object"' },
         ])
 
     expect(() => validate(object, null))
         .to.throw(ValidationError, 'Found 1 validation error')
         .with.property('errors').to.eql([
-          { key: '', message: 'Value is "null"' },
+          { path: [], message: 'Value is "null"' },
         ])
   })
 
@@ -30,27 +30,76 @@ describe('Array validators', () => {
     expect(() => validate(validator, 123))
         .to.throw(ValidationError, 'Found 1 validation error')
         .with.property('errors').to.eql([
-          { key: '', message: 'Value is not an "object"' },
+          { path: [], message: 'Value is not an "object"' },
         ])
 
     expect(() => validate(validator, null))
         .to.throw(ValidationError, 'Found 1 validation error')
         .with.property('errors').to.eql([
-          { key: '', message: 'Value is "null"' },
+          { path: [], message: 'Value is "null"' },
         ])
 
     expect(() => validate(validator, {}))
         .to.throw(ValidationError, 'Found 2 validation errors')
         .with.property('errors').to.eql([
-          { key: 'a', message: 'Required property missing' },
-          { key: 'b', message: 'Required property missing' },
+          { path: [ 'a' ], message: 'Required property missing' },
+          { path: [ 'b' ], message: 'Required property missing' },
         ])
 
-    expect(() => validate(validator, { a: 'foo', b: 1 }))
+    expect(() => validate(validator, { a: 'foo', b: 1, c: true }))
+        .to.throw(ValidationError, 'Found 3 validation errors')
+        .with.property('errors').to.eql([
+          { path: [ 'a' ], message: 'Value is not a "number"' },
+          { path: [ 'b' ], message: 'Value is not a "string"' },
+          { path: [ 'c' ], message: 'Unknown property found' },
+        ])
+  })
+
+  it('should validate a complex object', () => {
+    const contents = object({
+      description: string({ minLength: 1 }),
+      value: number,
+    })
+
+    const validator = object({
+      version: 1,
+      contents: array({ items: contents, minItems: 2 }),
+    } as const)
+
+    expect(validate(validator, {
+      version: 1,
+      contents: [ {
+        description: 'foo',
+        value: 123.456,
+      }, {
+        description: 'bar',
+        value: 654.321,
+      } ],
+    })).to.eql({
+      version: 1,
+      contents: [ {
+        description: 'foo',
+        value: 123.456,
+      }, {
+        description: 'bar',
+        value: 654.321,
+      } ],
+    })
+
+    expect(() => validate(validator, { version: 2, contents: [] }))
         .to.throw(ValidationError, 'Found 2 validation errors')
         .with.property('errors').to.eql([
-          { key: 'a', message: 'Value is not a "number"' },
-          { key: 'b', message: 'Value is not a "string"' },
+          { path: [ 'version' ], message: 'Value does not match constant "1"' },
+          { path: [ 'contents' ], message: 'Array must have a minimum length of 2' },
+        ])
+
+    expect(() => validate(validator, { version: 3, contents: [ {}, 'foo' ] }))
+        .to.throw(ValidationError, 'Found 4 validation errors')
+        .with.property('errors').to.eql([
+          { path: [ 'version' ], message: 'Value does not match constant "1"' },
+          { path: [ 'contents', 0, 'description' ], message: 'Required property missing' },
+          { path: [ 'contents', 0, 'value' ], message: 'Required property missing' },
+          { path: [ 'contents', 1 ], message: 'Value is not an "object"' },
         ])
   })
 })
