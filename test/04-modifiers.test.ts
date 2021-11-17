cf9db67b1f06dd7488e5d55ea1f8bf1b98cef52f
Pier Fumagalli <pier@usrz.com>
2021-11-17 18:28:57 +0100

    Validate optional properties

--- test/04-modifiers.test.ts
+++ test/04-modifiers.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { any, boolean, optional, readonly, string } from '../src'
+import { any, boolean, number, object, optional, readonly, string, validate, ValidationError } from '../src'
 
 describe('Object modifiers', () => {
   it('should construct a "readonly" modifier', () => {
@@ -78,5 +78,26 @@ describe('Object modifiers', () => {
       readonly: true,
       optional: true,
     })
+  })
+
+  it('should validate an object with optional properties', () => {
+    const schema1 = object({
+      foo: string,
+      bar: optional(number({ minimum: 50 })),
+      baz: readonly(number({ maximum: 50 })),
+    })
+
+    expect(validate(schema1, { foo: 'hello', bar: 50, baz: 50 }))
+        .to.eql({ foo: 'hello', bar: 50, baz: 50 })
+
+    expect(validate(schema1, { foo: 'hello', baz: 50 }))
+        .to.eql({ foo: 'hello', baz: 50 })
+
+    expect(() => validate(schema1, { foo: 'hello', bar: 40, baz: 60 }))
+        .to.throw(ValidationError, 'Found 2 validation errors')
+        .with.property('errors').eql([
+          { path: [ 'baz' ], message: 'Number is greater than 50' },
+          { path: [ 'bar' ], message: 'Number is less than 50' },
+        ])
   })
 })
