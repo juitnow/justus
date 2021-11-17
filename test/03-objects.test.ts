af27eb171be3fc803a92094a9b43fa3d47323834
Pier Fumagalli <pier@usrz.com>
2021-11-17 14:42:34 +0100

    Test additional properties and stripping

--- test/03-objects.test.ts
+++ test/03-objects.test.ts
@@ -1,4 +1,4 @@
-import { ValidationError, object, validate, string, number, array, allowAdditionalProperties, any } from '../src'
+import { ValidationError, object, validate, string, number, array, allowAdditionalProperties, any, never } from '../src'
 import { expect } from 'chai'
 
 describe('Object validator', () => {
@@ -56,18 +56,25 @@ describe('Object validator', () => {
   })
 
   it('should validate a complex object', () => {
+    const basic = object({
+      version: 0 as const,
+      title: string,
+    })
+
     const contents = object({
       description: string({ minLength: 1 }),
       value: number,
     })
 
     const validator = object({
+      ...basic.schema,
       version: 1,
       contents: array({ items: contents, minItems: 2 }),
     } as const)
 
     expect(validate(validator, {
       version: 1,
+      title: 'hello, world!',
       contents: [ {
         description: 'foo',
         value: 123.456,
@@ -77,6 +84,7 @@ describe('Object validator', () => {
       } ],
     })).to.eql({
       version: 1,
+      title: 'hello, world!',
       contents: [ {
         description: 'foo',
         value: 123.456,
@@ -87,16 +95,18 @@ describe('Object validator', () => {
     })
 
     expect(() => validate(validator, { version: 2, contents: [] }))
-        .to.throw(ValidationError, 'Found 2 validation errors')
+        .to.throw(ValidationError, 'Found 3 validation errors')
         .with.property('errors').to.eql([
           { path: [ 'version' ], message: 'Value does not match constant "1"' },
+          { path: [ 'title' ], message: 'Required property missing' },
           { path: [ 'contents' ], message: 'Array must have a minimum length of 2' },
         ])
 
     expect(() => validate(validator, { version: 3, contents: [ {}, 'foo' ] }))
-        .to.throw(ValidationError, 'Found 4 validation errors')
+        .to.throw(ValidationError, 'Found 5 validation errors')
         .with.property('errors').to.eql([
           { path: [ 'version' ], message: 'Value does not match constant "1"' },
+          { path: [ 'title' ], message: 'Required property missing' },
           { path: [ 'contents', 0, 'description' ], message: 'Required property missing' },
           { path: [ 'contents', 0, 'value' ], message: 'Required property missing' },
           { path: [ 'contents', 1 ], message: 'Value is not an "object"' },
@@ -104,32 +114,116 @@ describe('Object validator', () => {
   })
 
   it('should validate an object with additional properties', () => {
-    const validator1 = object({ foo: true, ...allowAdditionalProperties })
+    const validator1 = object({ foo: true, ...allowAdditionalProperties, baz: never })
     expect(validate(validator1, { foo: true, bar: 'whatever' }))
         .to.eql({ foo: true, bar: 'whatever' })
+    expect(() => validate(validator1, { foo: false, bar: 'whatever', baz: true }))
+        .to.throw(ValidationError, 'Found 2 validation errors')
+        .with.property('errors').to.eql([
+          { path: [ 'foo' ], message: 'Value does not match constant "true"' },
+          { path: [ 'baz' ], message: 'Forbidden property' },
+        ])
 
-    const validator2 = object({ foo: true, ...allowAdditionalProperties(true) })
+    const validator2 = object({ foo: true, ...allowAdditionalProperties(true), baz: never })
     expect(validate(validator2, { foo: true, bar: 'whatever' }))
         .to.eql({ foo: true, bar: 'whatever' })
+    expect(() => validate(validator2, { foo: false, bar: 'whatever', baz: true }))
+        .to.throw(ValidationError, 'Found 2 validation errors')
+        .with.property('errors').to.eql([
+          { path: [ 'foo' ], message: 'Value does not match constant "true"' },
+          { path: [ 'baz' ], message: 'Forbidden property' },
+        ])
 
-    const validator3 = object({ foo: true, ...allowAdditionalProperties(any) })
+    const validator3 = object({ foo: true, ...allowAdditionalProperties(any), baz: never })
     expect(validate(validator3, { foo: true, bar: 'whatever' }))
         .to.eql({ foo: true, bar: 'whatever' })
-
-    const validator4 = object({ foo: true, ...allowAdditionalProperties(false) })
-    expect(() => validate(validator4, { foo: true, bar: 'whatever' }))
-        .to.throw(ValidationError, 'Found 1 validation error')
+    expect(() => validate(validator3, { foo: false, bar: 'whatever', baz: true }))
+        .to.throw(ValidationError, 'Found 2 validation errors')
         .with.property('errors').to.eql([
+          { path: [ 'foo' ], message: 'Value does not match constant "true"' },
+          { path: [ 'baz' ], message: 'Forbidden property' },
+        ])
+
+    const validator4 = object({ foo: true, ...allowAdditionalProperties(false), baz: never })
+    expect(() => validate(validator4, { foo: true, bar: 'whatever', baz: true }))
+        .to.throw(ValidationError, 'Found 2 validation error')
+        .with.property('errors').to.eql([
+          { path: [ 'baz' ], message: 'Forbidden property' },
           { path: [ 'bar' ], message: 'Unknown property' },
         ])
 
-    const validator5 = object({ foo: true, ...allowAdditionalProperties(string) })
+    const validator5 = object({ foo: true, ...allowAdditionalProperties(string), baz: never })
     expect(validate(validator5, { foo: true, bar: 'whatever' }))
         .to.eql({ foo: true, bar: 'whatever' })
-    expect(() => validate(validator5, { foo: true, bar: 123 }))
+    expect(validate(validator5, { foo: true, bar: undefined, baz: undefined }))
+        .to.eql({ foo: true })
+        .to.have.keys([ 'foo' ]) // remove undefined props
+    expect(() => validate(validator5, { foo: true, bar: 123, baz: true }))
+        .to.throw(ValidationError, 'Found 2 validation errors')
+        .with.property('errors').to.eql([
+          { path: [ 'baz' ], message: 'Forbidden property' },
+          { path: [ 'bar' ], message: 'Value is not a "string"' },
+        ])
+  })
+
+  it('should strip additional or forbidden properties when asked to do so', () => {
+    const validator = object({ foo: true, baz: never })
+
+    // normal validation
+    expect(validate(validator, { foo: true }))
+        .to.eql({ foo: true })
+
+    // validate with forbidden/unknown properties are "undefined"
+    expect(validate(validator, { foo: true, bar: undefined, baz: undefined }))
+        .to.eql({ foo: true })
+        .to.have.keys([ 'foo' ])
+
+    // validate with forbidden/unknown properties set
+    expect(() => validate(validator, { foo: true, bar: 'whatever', baz: 123 }))
+        .to.throw(ValidationError, 'Found 2 validation errors')
+        .with.property('errors').to.eql([
+          { path: [ 'baz' ], message: 'Forbidden property' },
+          { path: [ 'bar' ], message: 'Unknown property' },
+        ])
+
+    // strip with unknown properties set
+    expect(validate(validator, { foo: true, bar: 'whatever' }, {
+      stripAdditionalProperties: true,
+    }))
+        .to.eql({ foo: true })
+        .to.have.keys([ 'foo' ])
+
+    // strip with forbidden properties set
+    expect(validate(validator, { foo: true, baz: 'whatever' }, {
+      stripForbiddenProperties: true,
+    }))
+        .to.eql({ foo: true })
+        .to.have.keys([ 'foo' ])
+
+    // strip with unknown/forbidden properties set
+    expect(validate(validator, { foo: true, bar: 'whatever', baz: 'forbidden' }, {
+      stripAdditionalProperties: true,
+      stripForbiddenProperties: true,
+    }))
+        .to.eql({ foo: true })
+        .to.have.keys([ 'foo' ])
+
+    // strip with forbidden properties set
+    expect(() => validate(validator, { foo: true, bar: 'whatever', baz: 'forbidden' }, {
+      stripAdditionalProperties: true,
+    }))
         .to.throw(ValidationError, 'Found 1 validation error')
         .with.property('errors').to.eql([
-          { path: [ 'bar' ], message: 'Value is not a "string"' },
+          { path: [ 'baz' ], message: 'Forbidden property' },
+        ])
+
+    // strip with forbidden properties set
+    expect(() => validate(validator, { foo: true, bar: 'whatever', baz: 'forbidden' }, {
+      stripForbiddenProperties: true,
+    }))
+        .to.throw(ValidationError, 'Found 1 validation error')
+        .with.property('errors').to.eql([
+          { path: [ 'bar' ], message: 'Unknown property' },
         ])
   })
 })
