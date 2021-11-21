c5a0b2772d9c0a7b8e2060e7f0b6b5aed330dbf4
Pier Fumagalli <pier@usrz.com>
2021-11-22 00:45:08 +0100

    More _elegant_ way to figure out the zero-length Validation constructor return type

--- src/validation.ts
+++ src/validation.ts
@@ -1,4 +1,4 @@
-import { ObjectValidator } from './objects'
+// import { ObjectValidator } from './objects'
 import { Validator } from './validator'
 import { getValidator } from './utilities'
 
@@ -13,9 +13,31 @@ export type Validation<V extends Validator = Validator> =
 
 /** Infer the type returned by a `Validator`'s own `validate` function. */
 export type InferValidationType<V extends Validation> =
-  V extends () => ObjectValidator ? Record<string, any> :
-  V extends ObjectValidator ? Record<string, any> :
-  V extends () => Validator<infer T> ? T :
+  // If this is a function, let's consider up to 10 overloads
+  V extends {
+    (...args: infer A0): Validator<infer R0>;
+    (...args: infer A1): Validator<infer R1>;
+    (...args: infer A2): Validator<infer R2>;
+    (...args: infer A3): Validator<infer R3>;
+    (...args: infer A4): Validator<infer R4>;
+    (...args: infer A5): Validator<infer R5>;
+    (...args: infer A6): Validator<infer R6>;
+    (...args: infer A7): Validator<infer R7>;
+    (...args: infer A8): Validator<infer R8>;
+    (...args: infer A9): Validator<infer R9>;
+  } ? // Consider only the overload with ZERO parameters
+    A0 extends [] ? R0 :
+    A1 extends [] ? R1 :
+    A2 extends [] ? R2 :
+    A3 extends [] ? R3 :
+    A4 extends [] ? R4 :
+    A5 extends [] ? R5 :
+    A6 extends [] ? R6 :
+    A7 extends [] ? R7 :
+    A8 extends [] ? R8 :
+    A9 extends [] ? R9 :
+    never :
+  // All the rest is normal stuff...
   V extends Validator<infer T> ? T :
   V // boolean, number, string or null
 
