7edfc151bcac7857000f5dd90eb772ec9f0b401b
Pier Fumagalli <pier@usrz.com>
2021-11-22 00:45:34 +0100

    Some sort of tuple validation

--- src/tuples.ts
+++ src/tuples.ts
@@ -1,6 +1,8 @@
-import { InferValidationType, Validation } from './validation'
+import { InferValidationType, Validation, ValidationOptions } from './validation'
 import { Validator } from './validator'
 import { tupleRest } from './symbols'
+import { getValidator, isValidation } from './utilities'
+import { assert } from './errors'
 
 export type TupleRest<V extends Validator = Validator> = {
   [tupleRest] : V
@@ -34,7 +36,41 @@ type EnforceTuple<T extends readonly (Validation | TupleRest)[]> =
     never :
   [ never ]
 
+
+export class TupleValidator<T extends Tuple> extends Validator<InferTuple<T>> {
+  readonly tupleValidators: readonly Validator[]
+  readonly restValidator?: Validator
+
+  constructor(tuple: EnforceTuple<T>) {
+    super()
+
+    const tupleValidators: Validator[] = []
+
+    for (let i = 0; i < tuple.length; i++) {
+      const item = tuple[i]
+
+      if (isValidation(item)) {
+        tupleValidators.push(getValidator(item))
+      } else if (tupleRest in item) {
+        if (i == (tuple.length - 1)) {
+          this.restValidator = item[tupleRest]
+        } else {
+          assert(false, '') // TODO
+        }
+      } else {
+        assert(false, '') // TODO
+      }
+    }
+
+    this.tupleValidators = tupleValidators
+  }
+
+  validate(value: unknown, options: ValidationOptions): InferTuple<T> {
+    void value, options
+    throw new Error('Method not implemented.') // TODO
+  }
+}
+
 export function tuple<T extends Tuple>(tuple: EnforceTuple<T>): Validator<InferTuple<T>> {
-  // TODO
-  return <any> tuple
+  return new TupleValidator(tuple)
 }
