dfbe0d68f15e5c012fc0772318406aa7b4170dd3
Pier Fumagalli <pier@usrz.com>
2021-11-25 00:15:20 +0100

    Export once

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,12 @@ export { ValidationError } from './errors'
 export * from './types'
 
 // Validators
-export { allOf, AllOfValidator } from './validators/union'
+export { allOf, oneOf, AllOfValidator, OneOfValidator } from './validators/union'
 export { any, AnyValidator } from './validators/any'
 export { array, arrayOf, ArrayValidator } from './validators/array'
 export { boolean, BooleanValidator } from './validators/boolean'
 export { constant, ConstantValidator } from './validators/constant'
 export { number, NumberValidator } from './validators/number'
-export { oneOf, OneOfValidator } from './validators/union'
 export { string, StringValidator } from './validators/string'
 export { tuple, TupleValidator } from './validators/tuple'
 
