6d9fe90a2d7d4a5fd6a33e7df8f834e597bd0541
Pier Fumagalli <pier@usrz.com>
2021-11-29 00:37:15 +0100

    Tuple validation

--- src/validators/tuple.ts
+++ src/validators/tuple.ts
@@ -1,16 +1,67 @@
-import { Tuple, InferTuple, Validator, ValidationOptions, TupleRestParameter, InferValidation, restValidator } from '../types'
+import { Tuple, InferTuple, Validator, TupleRestParameter, InferValidation, restValidator } from '../types'
+import { ValidationOptions } from '../types'
+import { assertValidation, ValidationErrorBuilder } from '../errors'
+import { getValidator } from '../utilities'
+import { nullValidator } from './constant'
+import { ValidationError } from '..'
 
 export class TupleValidator<T extends Tuple> extends Validator<InferTuple<T>> {
   readonly tuple: T
 
+  #validators: ({ single: boolean, validator: Validator })[] = []
+
   constructor(tuple: T) {
     super()
+
     this.tuple = tuple
+    for (const item of tuple) {
+      if (item === null) { // god knows why typeof null === "object"
+        this.#validators.push({ single: true, validator: nullValidator })
+      } else if ((typeof item === 'object') && (restValidator in item)) {
+        this.#validators.push({ single: false, validator: (<any>item)[restValidator] })
+      } else {
+        this.#validators.push({ single: true, validator: getValidator(item) })
+      }
+    }
   }
 
   validate(value: unknown, options: ValidationOptions): InferTuple<T> {
-    void value, options
-    throw new Error('Method not implemented.') // TODO
+    assertValidation(Array.isArray(value), 'Value is not an "array"')
+
+    // Empty tuples
+    if (this.#validators.length === 0) {
+      const size = value.length
+      assertValidation(size === 0, `Found ${size} element${size === 1 ? '' : 's'} validating empty tuple`)
+      return value as InferTuple<T>
+    }
+
+    // Validate iterating...
+    let needle = 0
+    let haystack = 0
+    let { single, validator } = this.#validators[needle]
+
+    while ((needle < this.#validators.length) && (haystack < value.length)) {
+      try {
+        validator.validate(value[haystack], options)
+        if (single) ({ single, validator } = this.#validators[++ needle] || {})
+        haystack ++
+      } catch (error) {
+        if (single) new ValidationErrorBuilder().record(error, haystack).assert(null)
+        else ({ single, validator } = this.#validators[++ needle] || {})
+      }
+    }
+
+    const missing = this.#validators.length - needle
+    if ((missing === 1) && single) {
+      throw new ValidationError('Tuple defines 1 missing validation')
+    } else if (missing > 1) {
+      throw new ValidationError(`Tuple defines ${missing} missing validations`)
+    }
+
+    const extra = value.length - haystack
+    assertValidation(extra === 0, `Found ${extra} extra element${extra === 1 ? '' : 's'} in tuple`)
+
+    return [ ...value ] as InferTuple<T>
   }
 }
 
