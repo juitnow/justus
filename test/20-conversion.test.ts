6ffe0d9f8fbeca8683572083e48b3de697f21dd6
Pier Fumagalli <pier@usrz.com>
2021-11-29 22:52:55 +0100

    Test cloning

---
+++ test/20-conversion.test.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai'
+import { arrayOf, date, object, tuple, validate } from '../src'
+
+describe('Type conversion', () => {
+  it('should convert types in arrays', () => {
+    const src = [ 0 ]
+    const tgt = validate(arrayOf(date), src)
+
+    expect(src).not.to.equal(tgt)
+    expect(src).to.eql([ 0 ])
+
+    expect(tgt).to.be.an('array').with.length(1)
+    expect(tgt[0]).to.be.instanceOf(Date)
+    expect(tgt[0].getTime()).to.equal(0)
+  })
+
+  it('should convert types in tuples', () => {
+    const src = [ 0 ]
+    const tgt = validate(tuple([ date ]), src)
+
+    expect(src).not.to.equal(tgt)
+    expect(src).to.eql([ 0 ])
+
+    expect(tgt).to.be.an('array').with.length(1)
+    expect(tgt[0]).to.be.instanceOf(Date)
+    expect(tgt[0].getTime()).to.equal(0)
+  })
+
+  it('should convert types in objects', () => {
+    const src = { foo: 0 }
+    const tgt = validate(object({ foo: date }), src)
+
+    expect(src).not.to.equal(tgt)
+    expect(src).to.eql({ foo: 0 })
+
+    expect(tgt).to.be.an('object')
+    expect(tgt.foo).to.be.instanceOf(Date)
+    expect(tgt.foo.getTime()).to.equal(0)
+  })
+})
