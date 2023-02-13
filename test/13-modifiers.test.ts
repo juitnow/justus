d7f4ff0c62ede053bc11643b06341f2612041483
Pier Fumagalli <pier@usrz.com>
2023-02-13 15:36:29 +0100

    Test _validators_ rather than _modifiers_.

--- test/13-modifiers.test.ts
+++ test/13-modifiers.test.ts
@@ -1,83 +1,16 @@
-import { any, boolean, modifierValidator, number, object, optional, readonly, string, strip, validate, ValidationError } from '../src/index'
+import { number, object, optional, readonly, string, strip, validate, ValidationError } from '../src/index'
 import { expect } from 'chai'
 
 describe('Object modifiers', () => {
-  it('should construct a "readonly" modifier', () => {
-    expect(readonly()).to.eql({
-      [modifierValidator]: any,
-      readonly: true,
-    })
-
-    expect(readonly(boolean)).to.eql({
-      [modifierValidator]: boolean,
-      readonly: true,
-    })
-
-    const validator = string
-
-    const modified = readonly(validator)
-    expect(modified).to.eql({
-      [modifierValidator]: validator,
-      readonly: true,
-    })
-  })
-
-  it('should construct a "optional" modifier', () => {
-    expect(optional()).to.eql({
-      [modifierValidator]: any,
-      optional: true,
-    })
-
-    expect(optional(boolean)).to.eql({
-      [modifierValidator]: boolean,
-      optional: true,
-    })
-
-    const validator = string
-
-    const modified = optional(validator)
-    expect(modified).to.eql({
-      [modifierValidator]: validator,
-      optional: true,
-    })
-  })
-
-  it('should combine modifiers', () => {
-    const validator = string
-
-    const r = readonly(validator)
-    expect(r).to.eql({
-      [modifierValidator]: validator,
-      readonly: true,
-    })
-
-    expect(readonly(r)).to.eql({
-      [modifierValidator]: validator,
-      readonly: true,
-    })
-
-    expect(optional(r)).to.eql({
-      [modifierValidator]: validator,
-      readonly: true,
-      optional: true,
-    })
-
-    const o = optional(validator)
-    expect(o).to.eql({
-      [modifierValidator]: validator,
-      optional: true,
-    })
-
-    expect(optional(o)).to.eql({
-      [modifierValidator]: validator,
-      optional: true,
-    })
-
-    expect(readonly(o)).to.eql({
-      [modifierValidator]: validator,
-      readonly: true,
-      optional: true,
-    })
+  it('should validate a simple optional validation', () => {
+    const validation = optional('foobar')
+    expect(validate(validation, undefined)).to.be.undefined
+    expect(validate(validation, 'foobar')).to.equal('foobar')
+    expect(() => validate(validation, 'wrong'))
+        .to.throw(ValidationError, 'Found 1 validation error')
+        .with.property('errors').eql([
+          { path: [], message: 'Value does not match constant "foobar"' },
+        ])
   })
 
   it('should validate an object with optional properties', () => {
