2f9f01f108caedf5b1886e4a0039f5a8bb296ce5
Pier Fumagalli <pier@usrz.com>
2023-02-13 02:24:15 +0100

    Use `AbstractValidator`

--- src/validators/boolean.ts
+++ src/validators/boolean.ts
@@ -1,8 +1,8 @@
-import { Validator } from '../types'
+import { AbstractValidator } from '../types'
 import { assertValidation } from '../errors'
 
 /** A `Validator` validating `boolean`s. */
-export class BooleanValidator extends Validator<boolean> {
+export class BooleanValidator extends AbstractValidator<boolean> {
   validate(value: unknown): boolean {
     assertValidation(typeof value === 'boolean', 'Value is not a "boolean"')
     return value
