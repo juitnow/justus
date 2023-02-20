3204382777b322dbcb4796a9c042438db4295d0a
Pier Fumagalli <pier@usrz.com>
2023-02-20 11:29:04 +0100

    Document the "pickle" we have with objects and generics...

--- test-d/07-generics.test-d.ts
+++ test-d/07-generics.test-d.ts
@@ -15,36 +15,30 @@ export class C<V1 extends Validation, V2 extends Validation> {
   test(value: unknown): void {
     const v1 = validate(this.validation1, value)
     expectType<InferValidation<V1>>(v1)
-    this.retest(v1)
 
     const v2 = validate(this.validation2, value)
     expectType<InferValidation<V2>>(v2)
-    expectError(this.retest(v2))
 
     const a1 = validate(arrayOf(this.validation1), [])
     expectType<InferValidation<V1>[]>(a1)
-    this.retest(...a1)
 
     const a2 = validate(arrayOf(this.validation2), [])
     expectType<InferValidation<V2>[]>(a2)
-    expectError(this.retest(...a2))
+
+    // objects with generics are tricky: we don't know whether the validated
+    // type inferred by the generic "validation" type in this class can extend
+    // "undefined" or not, so the key associated with the generic will _always_
+    // be marked as optional by our "InferSchema" type, and therefore the type
+    // will always be "InferValidation<...> | undefined"
 
     const o1 = validate(object({ test: this.validation1 }), value)
-    expectAssignable<{ test: InferValidation<V1> }>(o1)
-    expectType<InferValidation<V1>>(o1)
-    expectError(o1.nope)
-
-    this.retest(o1.test)
+    expectAssignable<{ test?: InferValidation<V1> | undefined }>(o1)
+    expectType<InferValidation<V1 | undefined>>(o1.test)
+    expectError(o1.nope) // just make sure we don't extend "any"
 
     const o2 = validate(object({ test: this.validation2 }), value)
-    expectAssignable<{ test: InferValidation<V2> }>(o2)
-    expectType<InferValidation<V2>>(o2)
-    expectError(o1.nope)
-
-    expectError(this.retest(o2.test))
-  }
-
-  retest(...value: InferValidation<V1>[]): void {
-    void value
+    expectAssignable<{ test?: InferValidation<V2> | undefined }>(o2)
+    expectType<InferValidation<V2> | undefined>(o2.test)
+    expectError(o2.nope) // just make sure we don't extend "any"
   }
 }
