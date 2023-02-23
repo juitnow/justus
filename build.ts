30ffa3ec3028d436ea37426aacfee9836c1e3569
Pier Fumagalli <pier@usrz.com>
2023-02-23 18:10:59 +0100

    Build updates: - correctly generate exports _only_ for `index` and `dts-generator` - include the `test-d` TSD test files in sources to lint

--- build.ts
+++ build.ts
@@ -1,3 +1,17 @@
-import { tasks } from '@plugjs/build'
+import { banner, build, find, merge, tasks } from '@plugjs/build'
 
-export default tasks()
+export default build({
+  ...tasks({
+    exportsGlob: '(index|dts-generator).*',
+  }),
+
+  /** Lint all sources (including `test-d` stuff) */
+  async lint(): Promise<void> {
+    banner('Linting sources')
+
+    await merge([
+      this._find_lint_sources(),
+      find('**/*.test-d.ts', { directory: 'test-d' }),
+    ]).eslint()
+  },
+})
