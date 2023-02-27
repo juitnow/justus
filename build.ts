522a990f123a62ad3f4f9f1d0f405f84b19c8381
Pier Fumagalli <pier@usrz.com>
2023-02-27 19:18:57 +0100

    Hook local tasks

--- build.ts
+++ build.ts
@@ -1,17 +1,26 @@
-import { banner, build, find, merge, tasks } from '@plugjs/build'
+import { banner, build, find, hookAfter, merge, tasks } from '@plugjs/build'
+import '@plugjs/tsd'
 
-export default build({
+const localBuild = build({
   ...tasks({
     exportsGlob: '(index|dts-generator).*',
   }),
 
-  /** Lint all sources (including `test-d` stuff) */
-  async lint(): Promise<void> {
-    banner('Linting sources')
+  /** Run `tsd` */
+  async tsd(): Promise<void> {
+    banner('Testing type definitions')
+    await find('**/*.test-d.ts', { directory: 'test-d' }).tsd()
+  },
 
-    await merge([
-      this._find_lint_sources(),
-      find('**/*.test-d.ts', { directory: 'test-d' }),
-    ]).eslint()
+  /** Lint all sources (including `test-d` stuff) */
+  async lint_tsd(): Promise<void> {
+    banner('Linting type defintion test sources')
+
+    await find('**/*.test-d.ts', { directory: 'test-d' }).eslint()
   },
 })
+
+hookAfter(localBuild, 'test', [ 'tsd' ])
+hookAfter(localBuild, 'lint', [ 'lint_tsd' ])
+
+export default localBuild
