8fa568e9db487560b97402ef7bbb3efb6a8391f3
Pier Fumagalli <pier@usrz.com>
2023-08-27 01:15:25 +0200

    Extra types validation rework (and add ARN tests)

--- test-d/20-extras.test-d.ts
+++ test-d/20-extras.test-d.ts
@@ -1,10 +1,14 @@
 import { expectType, printType } from 'tsd'
 
 import { validate } from '../src'
+import { arn, parseArn } from '../src/extra/arn'
 import { ean13 } from '../src/extra/ean13'
 import { uuid } from '../src/extra/uuid'
 
 import type { InferInput } from '../src'
+import type { ArnString, ParsedArn } from '../src/extra/arn'
+import type { EAN13String } from '../src/extra/ean13'
+import type { UUIDString } from '../src/extra/uuid'
 
 printType('__file_marker__')
 
@@ -12,8 +16,28 @@ function inputType<T>(param: T): InferInput<T> {
   throw new Error(`${param}`)
 }
 
-expectType<string & { __brand_ean_13: never }>(validate(ean13, 'foo'))
+/* ===== ARNs =============================================================== */
+
+expectType<string & { __arn: never }>(validate(arn, 'foo'))
+expectType<string & ArnString>(validate(arn, 'foo'))
+
+expectType<string & { __arn: never } & { __arn_myservice: never }>(validate(arn('myservice'), 'foo'))
+expectType<ArnString<'myservice'>>(validate(arn('myservice'), 'foo'))
+
+expectType<ParsedArn<string>>(validate(parseArn, 'foo'))
+expectType<ParsedArn<'myservice'>>(validate(parseArn('myservice'), 'foo'))
+
+expectType<string>(inputType(arn))
+expectType<string>(inputType(parseArn))
+
+/* ===== EAN-13s ============================================================ */
+
+expectType<string & { __ean_13: never }>(validate(ean13, 'foo'))
+expectType<EAN13String>(validate(ean13, 'foo'))
 expectType<string | number>(inputType(ean13))
 
-expectType<string & { __brand_uuid: never }>(validate(uuid, 'foo'))
+/* ===== UUIDs ============================================================== */
+
+expectType<string & { __uuid: never }>(validate(uuid, 'foo'))
+expectType<UUIDString>(validate(uuid, 'foo'))
 expectType<string>(inputType(uuid))
