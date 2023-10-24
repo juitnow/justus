6de4341dcffa69f0ab711c89ff21243272d8668b
Pier Fumagalli <pier@usrz.com>
2023-10-24 23:09:32 +0200

    Test big integer validation

--- test/10-primitives.test.ts
+++ test/10-primitives.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 
-import { any, boolean, constant, number, NumberValidator, string, StringValidator, validate, ValidationError } from '../src'
+import { any, bigint, BigIntValidator, boolean, constant, number, NumberValidator, string, StringValidator, validate, ValidationError } from '../src'
 
 describe('Primitive validators', () => {
   describe('any', () => {
@@ -320,6 +320,168 @@ describe('Primitive validators', () => {
     it('should support implicit branding', () => {
       const validator = string({ brand: 'foo' })
       expect(validate(validator, 'foo')).to.equal('foo')
+      expect(validator.brand).to.equal('foo')
+    })
+  })
+
+  describe('bigint', () => {
+    it('should validate a bigint', () => {
+      expect(validate(bigint, 0n)).to.equal(0n)
+      expect(validate(bigint, 123n)).to.equal(123n)
+
+      expect(validate(bigint, 0)).to.equal(0n)
+      expect(validate(bigint, 123)).to.equal(123n)
+
+      expect(() => validate(bigint, 123.456))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt can not be parsed from number' },
+          ])
+
+      expect(() => validate(bigint, '12345'))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'Value is not a "bigint"' },
+          ])
+    })
+
+    it('should validate a bigint within a range', () => {
+      const range = bigint({ minimum: -9, maximum: 19 })
+      expect(validate(range, 0)).to.equal(0n)
+      expect(validate(range, -9)).to.equal(-9n)
+      expect(validate(range, 19)).to.equal(19n)
+
+      expect(validate(bigint({ minimum: 0, maximum: 0 }), 0)).to.equal(0n)
+
+      expect(() => validate(range, -10))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is less than -9' },
+          ])
+
+      expect(() => validate(range, 20))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is greater than 19' },
+          ])
+
+      expect(() => bigint({ minimum: 10, maximum: 9 }))
+          .to.throw(TypeError, 'Constraint "minimum" (10) is greater than "maximum" (9)')
+    })
+
+    it('should validate a bigint within an exclusive range', () => {
+      const range1 = bigint({ exclusiveMinimum: 0, exclusiveMaximum: 10 })
+      expect(validate(range1, 1)).to.equal(1n)
+      expect(validate(range1, 9)).to.equal(9n)
+
+      expect(() => validate(range1, 0))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is less than or equal to 0' },
+          ])
+
+      expect(() => validate(range1, 10))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is greater than or equal to 10' },
+          ])
+
+      const range2 = bigint({ exclusiveMinimum: 0, maximum: 10 })
+      expect(validate(range2, 1)).to.equal(1n)
+      expect(validate(range2, 10)).to.equal(10n)
+
+      expect(() => validate(range2, 0))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is less than or equal to 0' },
+          ])
+
+      expect(() => validate(range2, 11))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is greater than 10' },
+          ])
+
+      const range3 = bigint({ minimum: 0, exclusiveMaximum: 10 })
+      expect(validate(range3, 0)).to.equal(0n)
+      expect(validate(range3, 9)).to.equal(9n)
+
+      expect(() => validate(range3, -1))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is less than 0' },
+          ])
+
+      expect(() => validate(range3, 10))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is greater than or equal to 10' },
+          ])
+
+      expect(() => bigint({ exclusiveMinimum: 2, exclusiveMaximum: 1 }))
+          .to.throw(TypeError, 'Constraint "exclusiveMaximum" (1) must be greater than "exclusiveMinimum" (2)')
+      expect(() => bigint({ exclusiveMinimum: 0, exclusiveMaximum: 0 }))
+          .to.throw(TypeError, 'Constraint "exclusiveMaximum" (0) must be greater than "exclusiveMinimum" (0)')
+
+      expect(() => bigint({ exclusiveMinimum: 2, maximum: 1 }))
+          .to.throw(TypeError, 'Constraint "maximum" (1) must be greater than "exclusiveMinimum" (2)')
+      expect(() => bigint({ exclusiveMinimum: 0, maximum: 0 }))
+          .to.throw(TypeError, 'Constraint "maximum" (0) must be greater than "exclusiveMinimum" (0)')
+
+      expect(() => bigint({ minimum: 2, exclusiveMaximum: 1 }))
+          .to.throw(TypeError, 'Constraint "exclusiveMaximum" (1) must be greater than "minimum" (2)')
+      expect(() => bigint({ minimum: 0, exclusiveMaximum: 0 }))
+          .to.throw(TypeError, 'Constraint "exclusiveMaximum" (0) must be greater than "minimum" (0)')
+    })
+
+    it('should validate multiple ofs', () => {
+      expect(validate(bigint({ multipleOf: 2 }), 4)).to.equal(4n)
+      expect(() => validate(bigint({ multipleOf: 2 }), 3))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt is not a multiple of 2' },
+          ])
+    })
+
+    it('should validate a bigint parsing a string', () => {
+      const validator = bigint({ fromString: true })
+      expect(validate(validator, '123456'), '123456').to.equal(123456n)
+      expect(validate(validator, '0xCAFE'), '0xCAFE').to.equal(0xCAFEn)
+      expect(validate(validator, '0o4321'), '0o4321').to.equal(0o4321n)
+      expect(validate(validator, '0b1011'), '0b1011').to.equal(0b1011n)
+      expect(() => validate(validator, '123.456'))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt can not be parsed from string' },
+          ])
+    })
+
+    it('should validate a bigint parsing a number', () => {
+      const validator = bigint({ fromNumber: true })
+      expect(validate(validator, 123456), '123456').to.equal(123456n)
+
+      expect(() => validate(validator, 123.456))
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'BigInt can not be parsed from number' },
+          ])
+
+      const validator2 = bigint({ fromNumber: false })
+      expect(() => validate(validator2, 123456), '123456')
+          .to.throw(ValidationError, 'Found 1 validation error')
+          .with.property('errors').to.eql([
+            { path: [], message: 'Value is not a "bigint"' },
+          ])
+    })
+
+    it('should validate a bigint using the BigIntValidator class', () => {
+      const validator = new BigIntValidator()
+      expect(validate(validator, 12345n)).to.equal(12345n)
+    })
+
+    it('should support implicit branding', () => {
+      const validator = bigint({ brand: 'foo' })
+      expect(validate(validator, 12345n)).to.equal(12345n)
       expect(validator.brand).to.equal('foo')
     })
   })
