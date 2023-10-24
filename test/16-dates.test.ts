ff4fc3e0ae0e5755240e0c869eab9594a1ba8eb6
Pier Fumagalli <pier@usrz.com>
2023-10-25 00:52:48 +0200

    Convert to Expect5

--- test/16-dates.test.ts
+++ test/16-dates.test.ts
@@ -1,93 +1,99 @@
-import { expect } from 'chai'
-
 import { date, validate, ValidationError } from '../src'
 
 describe('Date validator', () => {
   it('should validate a simple date', () => {
-    expect(validate(date, 0).getTime()).to.eql(0)
-    expect(validate(date, '1970-01-01T00:00:00.000Z').getTime()).to.eql(0)
-    expect(validate(date, 'Thu, 01 Jan 1970 00:00:00 GMT').getTime()).to.eql(0)
+    expect(validate(date, 0).getTime()).toEqual(0)
+    expect(validate(date, '1970-01-01T00:00:00.000Z').getTime()).toEqual(0)
+    expect(validate(date, 'Thu, 01 Jan 1970 00:00:00 GMT').getTime()).toEqual(0)
   })
 
   it('should validate a Date instance', () => {
     const now = new Date()
-    expect(validate(date, now).getTime()).to.eql(now.getTime())
+    expect(validate(date, now).getTime()).toEqual(now.getTime())
   })
 
   it('should validate an ISO date', () => {
     const validator = date({ format: 'iso' })
 
-    expect(validate(validator, '1970-01-01T00:00:00.000Z').getTime()).to.eql(0)
+    expect(validate(validator, '1970-01-01T00:00:00.000Z').getTime()).toEqual(0)
 
     expect(() => validate(validator, 'Thu, 01 Jan 1970 00:00:00 GMT'))
-        .to.throw(ValidationError, /^Found 1 validation error/)
-        .with.property('errors').to.eql([ {
-          path: [], message: 'Invalid format for ISO Date',
-        } ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 1 validation error/)
+            .toHaveProperty('errors', expect.toMatchContents([ {
+              path: [], message: 'Invalid format for ISO Date',
+            } ])))
   })
 
   it('should validate a timestamp', () => {
     const validator = date({ format: 'timestamp' })
 
-    expect(validate(validator, 0).getTime()).to.eql(0)
+    expect(validate(validator, 0).getTime()).toEqual(0)
 
     expect(() => validate(validator, 'Thu, 01 Jan 1970 00:00:00 GMT'))
-        .to.throw(ValidationError, /^Found 1 validation error/)
-        .with.property('errors').to.eql([ {
-          path: [], message: 'Timestamp is not a "number"',
-        } ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 1 validation error/)
+            .toHaveProperty('errors', expect.toMatchContents([ {
+              path: [], message: 'Timestamp is not a "number"',
+            } ])))
   })
 
   it('should validate a date within a range', () => {
     const v1 = date({ from: new Date(2000), until: new Date(3000) })
 
-    expect(validate(v1, 2000).getTime()).to.eql(2000)
-    expect(validate(v1, 3000).getTime()).to.eql(3000)
+    expect(validate(v1, 2000).getTime()).toEqual(2000)
+    expect(validate(v1, 3000).getTime()).toEqual(3000)
 
     expect(() => validate(v1, 1999))
-        .to.throw(ValidationError, /^Found 1 validation error/)
-        .with.property('errors').to.eql([ {
-          path: [], message: 'Date is before 1970-01-01T00:00:02.000Z',
-        } ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 1 validation error/)
+            .toHaveProperty('errors', expect.toMatchContents([ {
+              path: [], message: 'Date is before 1970-01-01T00:00:02.000Z',
+            } ])))
 
     expect(() => validate(v1, 3001))
-        .to.throw(ValidationError, /^Found 1 validation error/)
-        .with.property('errors').to.eql([ {
-          path: [], message: 'Date is after 1970-01-01T00:00:03.000Z',
-        } ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 1 validation error/)
+            .toHaveProperty('errors', expect.toMatchContents([ {
+              path: [], message: 'Date is after 1970-01-01T00:00:03.000Z',
+            } ])))
 
 
     const v2 = date({ from: new Date(2000), until: new Date(2000) })
 
-    expect(validate(v2, 2000).getTime()).to.eql(2000)
+    expect(validate(v2, 2000).getTime()).toEqual(2000)
 
     expect(() => validate(v2, 1999))
-        .to.throw(ValidationError, /^Found 1 validation error/)
-        .with.property('errors').to.eql([ {
-          path: [], message: 'Date is before 1970-01-01T00:00:02.000Z',
-        } ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 1 validation error/)
+            .toHaveProperty('errors', expect.toMatchContents([ {
+              path: [], message: 'Date is before 1970-01-01T00:00:02.000Z',
+            } ])))
 
     expect(() => validate(v2, 2001))
-        .to.throw(ValidationError, /^Found 1 validation error/)
-        .with.property('errors').to.eql([ {
-          path: [], message: 'Date is after 1970-01-01T00:00:02.000Z',
-        } ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 1 validation error/)
+            .toHaveProperty('errors', expect.toMatchContents([ {
+              path: [], message: 'Date is after 1970-01-01T00:00:02.000Z',
+            } ])))
 
     expect(() => date({ from: new Date(100), until: new Date(99) }))
-        .to.throw(TypeError, 'Constraint "until" (1970-01-01T00:00:00.099Z) must not be before "from" (1970-01-01T00:00:00.100Z)')
+        .toThrowError(TypeError, 'Constraint "until" (1970-01-01T00:00:00.099Z) must not be before "from" (1970-01-01T00:00:00.100Z)')
   })
 
   it('should consider edge cases', () => {
     expect(() => validate(date, Symbol()))
-        .to.throw(ValidationError, /^Found 1 validation error/)
-        .with.property('errors').to.eql([ {
-          path: [], message: 'Value could not be converted to a "Date"',
-        } ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 1 validation error/)
+            .toHaveProperty('errors', expect.toMatchContents([ {
+              path: [], message: 'Value could not be converted to a "Date"',
+            } ])))
 
     expect(() => validate(date, Number.POSITIVE_INFINITY))
-        .to.throw(ValidationError, /^Found 1 validation error/)
-        .with.property('errors').to.eql([ {
-          path: [], message: 'Invalid date',
-        } ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 1 validation error/)
+            .toHaveProperty('errors', expect.toMatchContents([ {
+              path: [], message: 'Invalid date',
+            } ])))
   })
 })
--- test/50-extra-url.test.ts
+++ test/50-extra-url.test.ts
@@ -1,18 +1,16 @@
-import { expect } from 'chai'
-
 import { constant, optional, string, validate, ValidationError } from '../src'
 import { url } from '../src/extra/url'
 
 describe('URL validator', () => {
   it('should validate a simple URL', () => {
-    expect(validate(url, 'http://www/').href).to.equal('http://www/')
-    expect(validate(url, 'foo:bar/baz').href).to.equal('foo:bar/baz')
-    expect(validate(url, new URL('http://www/')).href).to.equal('http://www/')
+    expect(validate(url, 'http://www/').href).toStrictlyEqual('http://www/')
+    expect(validate(url, 'foo:bar/baz').href).toStrictlyEqual('foo:bar/baz')
+    expect(validate(url, new URL('http://www/')).href).toStrictlyEqual('http://www/')
   })
 
   it('should not validate a wrong URL', () => {
     expect(() => validate(url, '/foo/bar'))
-        .to.throw(ValidationError, 'Value could not be converted to a "URL"')
+        .toThrowError(ValidationError, /Value could not be converted to a "URL"/)
   })
 
   it('should validate an URL with some basic constraints', () => {
@@ -23,15 +21,16 @@ describe('URL validator', () => {
       hash: undefined, // have the key but no value
     })
 
-    expect(validator.validate('http://www/a/b/c#foo').href).to.equal('http://www/a/b/c#foo')
+    expect(validator.validate('http://www/a/b/c#foo').href).toStrictlyEqual('http://www/a/b/c#foo')
 
     expect(() => validator.validate('ftp://funet/fi'))
-        .to.throw(ValidationError, /^Found 3 validation errors/)
-        .with.property('errors').to.eql([
-          { path: [ 'protocol' ], message: 'String does not match required pattern /^https?:$/' },
-          { path: [ 'hostname' ], message: 'Value does not match constant "www" (string)' },
-          { path: [ 'pathname' ], message: 'Value does not match constant "/a/b/c" (string)' },
-        ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 3 validation errors/)
+            .toHaveProperty('errors', expect.toMatchContents([
+              { path: [ 'protocol' ], message: 'String does not match required pattern /^https?:$/' },
+              { path: [ 'hostname' ], message: 'Value does not match constant "www" (string)' },
+              { path: [ 'pathname' ], message: 'Value does not match constant "/a/b/c" (string)' },
+            ])))
   })
 
   it('should validate an URL with some search parameters constraints', () => {
@@ -42,12 +41,13 @@ describe('URL validator', () => {
       },
     })
 
-    expect(validator.validate('http://www/?foo=bar&baz=xyz').href).to.eql('http://www/?foo=bar&baz=xyz')
+    expect(validator.validate('http://www/?foo=bar&baz=xyz').href).toEqual('http://www/?foo=bar&baz=xyz')
     expect(() => validator.validate('http://www/?baz=abc').href)
-        .to.throw(ValidationError, /^Found 2 validation errors/)
-        .with.property('errors').to.eql([
-          { path: [ 'searchParams', 'foo' ], message: 'Required property missing' },
-          { path: [ 'searchParams', 'baz' ], message: 'Value does not match constant "xyz" (string)' },
-        ])
+        .toThrow((assert) => assert
+            .toBeError(ValidationError, /^Found 2 validation errors/)
+            .toHaveProperty('errors', expect.toMatchContents([
+              { path: [ 'searchParams', 'foo' ], message: 'Required property missing' },
+              { path: [ 'searchParams', 'baz' ], message: 'Value does not match constant "xyz" (string)' },
+            ])))
   })
 })
