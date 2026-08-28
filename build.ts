de1b9aba01fee43684b2e64d3172bc1b62134d4f
Pier Fumagalli <pier@usrz.com>
2026-08-28 16:30:28 +0200

    New build file (ES-only)

--- build.ts
+++ build.ts
@@ -1,24 +1,31 @@
-import {
-  banner,
-  exec,
-  find,
-  hookAfter,
-  log,
-  mkdtemp,
-  plugjs,
-  resolve,
-  rmrf,
-  tasks,
-} from '@plugjs/build'
+import '@plugjs/cov8'
+import '@plugjs/expect5'
+import { oxc } from '@plugjs/oxc'
+import { banner, find, isDirectory, plugjs, rmrf } from '@plugjs/plug'
 import '@plugjs/tsd'
+import { tsc } from '@plugjs/tsgo'
 
-const localBuild = plugjs({
-  ...tasks({
-    exportsGlob: '((index|dts-generator).*)|(extra/**.*)',
-    extraLint: [ [ '**/*.test-d.ts', { directory: 'test-d' } ] ],
-  }),
+export default plugjs({
+  /** Flag indicating whether linting/formatting errors should be fixed or not */
+  fix: 'false',
 
-  /** Run `tsd` */
+  /** Transpile all sources and produce some JavaScript code in 'dist' */
+  async transpile(): Promise<void> {
+    banner('Transpiling TypeScript Sources')
+    if (isDirectory('dist')) await rmrf('dist')
+    await tsc('src/tsconfig.json')
+  },
+
+  /** Run all tests */
+  async test(): Promise<void> {
+    banner('Running Tests')
+    await rmrf('.coverage-data') // wipe existing coverage data
+    // await tsc('test/tsconfig.json', { noEmit: true }) // type check our tests
+    await find('**/*.test.*', { directory: 'test' }) // find all our test files
+      .test({ coverageDir: '.coverage-data' }) // run tests
+  },
+
+  /** Run type definition tests */
   async tsd(): Promise<void> {
     await this.transpile()
 
@@ -28,30 +35,40 @@ const localBuild = plugjs({
     })
   },
 
-  /** Text compilation of *dependant* packages */
-  async test_dependants(): Promise<void> {
-    await this.transpile()
-
-    banner('Testing transpilation of dependant packages')
-    const tmpdir = mkdtemp()
+  /** Run tests capturing errors, but always producing coverage */
+  async coverage(): Promise<void> {
     try {
-      // copy files to the temporary directory
-      await find('**/*', { directory: 'test/sources' })
-          .copy(tmpdir)
-
-      // instell ourselves as a module in the temporary directory
-      await exec('npm', 'install', '--no-save', '--prefix', tmpdir, resolve('.'))
-
-      // run tsc to make sure that all types are exported correctly
-      await find('**/*.ts', { directory: tmpdir }).tsc()
-
-      log('Sources compiled successfully')
+      await this.test()
     } finally {
-      await rmrf(tmpdir)
+      banner('Preparing coverage report')
+      await find('**/*.ts', { directory: 'src', ignore: 'configs/*.ts' }) //
+        .coverage('.coverage-data', {
+          reportDir: 'coverage',
+          minimumCoverage: 100,
+          minimumFileCoverage: 100,
+        })
     }
   },
+
+  /** Format and lint all our sources (optionally fixing them) */
+  async lint(): Promise<void> {
+    banner('Linting Sources')
+    await oxc({ fix: this.fix === 'true' })
+  },
+
+  /** Generate our exports from the list of transpiled files */
+  async exports(): Promise<void> {
+    await this.transpile()
+
+    banner('Generating Exports')
+    await find('index.*', 'dts-generator.*', 'extra/**.*', { directory: 'dist' }).exports({})
+  },
+
+  /** Default task: transpile, test and lint */
+  async default(): Promise<void> {
+    await this.transpile()
+    await this.tsd()
+    await this.coverage()
+    await this.lint()
+  },
 })
-
-hookAfter(localBuild, 'test', [ 'tsd', 'test_dependants' ])
-
-export default localBuild
